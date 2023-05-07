ccaf3cf1a27312c70493fe5cef1aafa147595eec
kishioka-yoichi <youichipanda@gmail.com>
2023-05-07 21:07:33 +0900

    test2

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     String name = "test";
+    int age = 129;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
